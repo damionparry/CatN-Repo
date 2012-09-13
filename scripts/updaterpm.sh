f8d9dbf670a457e326152ef29170d3d71d193c7b
Nicola Asuni <nicola@fubra.com>
2012-09-13 13:22:19 +0100

    'CentOS 2.6.32-279.5.2.el6.x86_64'

--- scripts/updaterpm.sh
+++ scripts/updaterpm.sh
@@ -299,8 +299,7 @@ ssh root@$RPMHOST "echo '    rpm_logpipe: logpipe-'$LPVER'-'$FVER'' >> /home/mak
 
 ssh root@$RPMHOST "rm -rf /home/makerpm/ansible"
 ssh root@$RPMHOST "su -c 'cd /home/makerpm && git clone --branch=yum-install-rpm git://github.com/dhozac/ansible.git' makerpm"
-ssh root@$RPMHOST "cd /home/makerpm/ansible"
-ssh root@$RPMHOST "make rpm"
+ssh root@$RPMHOST "su -c 'cd /home/makerpm/ansible && make rpm' makerpm"
 
 # ..............................................................................
 # ..............................................................................
